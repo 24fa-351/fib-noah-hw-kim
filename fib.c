971e375e60cecd66f2c340cb33dd5d9badec3370
Noah Kim <noah.hw.kim@gmail.com>
2024-11-20 15:29:55 -0800

    1) added clang-format file 2) updated fib.c to check argc before argv

--- fib.c
+++ fib.c
@@ -11,11 +11,15 @@ unsigned long long int fib_r(int);
 unsigned long long int fib_wrapper(int);
 void initializeCache(ProviderPtr);
 
-unsigned long long int fib_i(int seq) {
+unsigned long long int fib_i(int seq)
+{
    // base
-   if (seq == 1) {
+   if (seq == 1)
+   {
       return 0;
-   } else if (seq == 2) {
+   }
+   else if (seq == 2)
+   {
       return 1;
    }
 
@@ -23,7 +27,8 @@ unsigned long long int fib_i(int seq) {
    unsigned long long int cur = 1;
 
    // iterate starts from 3rd fib since we already have 1st and 2nd fib
-   for (int i = 0; i < seq - 2; i++) {
+   for (int i = 0; i < seq - 2; i++)
+   {
       unsigned long long int nxt = prev + cur;
       prev = cur;
       cur = nxt;
@@ -32,11 +37,15 @@ unsigned long long int fib_i(int seq) {
    return cur;
 }
 
-unsigned long long int fib_r(int seq) {
+unsigned long long int fib_r(int seq)
+{
    // base case
-   if (seq == 1) {
+   if (seq == 1)
+   {
       return 0;
-   } else if (seq == 2) {
+   }
+   else if (seq == 2)
+   {
       return 1;
    }
 
@@ -45,15 +54,18 @@ unsigned long long int fib_r(int seq) {
    return cur;
 }
 
-void initializeCache(ProviderPtr fib) {
+void initializeCache(ProviderPtr fib)
+{
    ptr = fib;
    // memo[1] = 0;
    memo[2] = 1;
 }
 
-unsigned long long int fib_wrapper(int seq) {
+unsigned long long int fib_wrapper(int seq)
+{
    // recursive case
-   if (memo[seq] != 0) {
+   if (memo[seq] != 0)
+   {
       return memo[seq];
    }
 
@@ -62,11 +74,18 @@ unsigned long long int fib_wrapper(int seq) {
    return memo[seq];
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+   if (argc != 4)
+   {
+      printf("Usage: %s <number> <method> <filename>\n", argv[0]);
+      return 1;
+   }
+
    int num1;
    sscanf(argv[1], "%d", &num1);
-   char* method = argv[2];
-   char* fileName = argv[3];
+   char *method = argv[2];
+   char *fileName = argv[3];
 
    int num2 = 0;
 
@@ -76,11 +95,14 @@ int main(int argc, char *argv[]) {
    fclose(f);
 
    unsigned long long int result;
-   
+
    // nth number starts from 1 not 0
-   if (*method == 'r') {
+   if (*method == 'r')
+   {
       initializeCache(fib_r);
-   } else if (*method == 'i') {
+   }
+   else if (*method == 'i')
+   {
       initializeCache(fib_i);
    }
 
